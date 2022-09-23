9d28f5d1ddaad9ac6300ff31c04b5bd321048375
Akhmad Ramadani <akhmdramadani18@gmail.com>
2022-09-23 16:48:10 +0700

    homework

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Percobaan Pertama
 
 <img src="images/ss1.png" alt="ss 1" style="width:200px;"/>
+
+HomeWork
+
+<img src="images/ss2.png" alt="ss 2" style="width:200px;"/>
+<img src="images/ss3.png" alt="ss 3" style="width:200px;"/>
--- app/src/main/java/jti/akhmad/ramadani/a04_android_recyclerview/MainActivity.java
+++ app/src/main/java/jti/akhmad/ramadani/a04_android_recyclerview/MainActivity.java
@@ -1,5 +1,6 @@
 package jti.akhmad.ramadani.a04_android_recyclerview;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -12,15 +13,19 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import jti.akhmad.ramadani.a04_android_recyclerview.Adapters.RecipeListAdapter;
 import jti.akhmad.ramadani.a04_android_recyclerview.Adapters.WordListAdapter;
+import jti.akhmad.ramadani.a04_android_recyclerview.Models.RecipeData;
 import jti.akhmad.ramadani.a04_android_recyclerview.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private RecyclerView mRecyclerView;
     private WordListAdapter mAdapter;
+
+    private ArrayList<RecipeData> recipeList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,32 +47,50 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int wordListSize = mWordList.size();
-                // Add a new word to the wordList.
-                mWordList.addLast("+ Word " + wordListSize);
-                // Notify the adapter, that the data has changed.
-                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
-                // Scroll to the bottom.
-                mRecyclerView.smoothScrollToPosition(wordListSize);
-            }
-        });
-
-        for (int i = 0; i < 20; i++) {
-            mWordList.addLast("Word " + i);
-        }
+        setRecipeList();
 
         mRecyclerView = findViewById(R.id.recyclerview);
         mAdapter = new WordListAdapter(this, mWordList);
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecipeListAdapter recipeListAdapter = new RecipeListAdapter(MainActivity.this, recipeList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(recipeListAdapter);
+        recipeListAdapter.setOnItemClickListener(onItemClickListener);
     }
+
+    private void setRecipeList() {
+        recipeList = new ArrayList<>();
+        RecipeData data;
+        data = new RecipeData(getString(R.string.moo_shu_name), getString(R.string.moo_shu_description), R.drawable.moo_shu_img, getString(R.string.moo_shu_details));
+        recipeList.add(data);
+        data = new RecipeData(getString(R.string.grilled_shrimp_name), getString(R.string.grilled_shrimp_description), R.drawable.grilled_shrimp_img, getString(R.string.grilled_shrimp_details));
+        recipeList.add(data);
+        data = new RecipeData(getString(R.string.sirloin_tips_name), getString(R.string.sirloin_tips_description), R.drawable.sirloin_tips_img, getString(R.string.sirloin_tips_details));
+        recipeList.add(data);
+        data = new RecipeData(getString(R.string.squash_casserole_name), getString(R.string.squash_casserole_description), R.drawable.squash_casserole_img, getString(R.string.squash_casserole_details));
+        recipeList.add(data);
+        data = new RecipeData(getString(R.string.slow_casserole_name), getString(R.string.slow_casserole_description), R.drawable.slow_casserole_img, getString(R.string.slow_casserole_details));
+        recipeList.add(data);
+
+    }
+
+    public void openDetailActivity(int imageId, String details){
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        intent.putExtra("image", imageId);
+        intent.putExtra("details", details);
+        startActivity(intent);
+    }
+
+    private View.OnClickListener onItemClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RecyclerView.ViewHolder viewHolder = (RecyclerView.ViewHolder) v.getTag();
+            int position = viewHolder.getAdapterPosition();
+            RecipeData thisRecipe = recipeList.get(position);
+            openDetailActivity(thisRecipe.getImage(), thisRecipe.getDetails());
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
